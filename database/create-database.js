2f60e347b9da4f505adc39c9f5c85bdc361de93e
david-go <12720260+david-go@user.noreply.gitee.com>
2024-04-29 19:19:50 +0200

    festo data

--- database/create-database.js
+++ database/create-database.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Cool123456789',
+  password: 'Cool1234567890-',
   database: 'bmg'
 });
 
